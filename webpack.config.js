f4f198ff0ae3ecb779f51bc82088b5676084701a
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-14 15:53:11 -0300

    add: index

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 module.exports = {
     entry: './ex/index.js',
     output: {
-        path: _dirname + '/public',
+        path: __dirname + '/public',
         filename: './bundle.js' 
 },
     devServer: {
